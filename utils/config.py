f6a031ae5591fce17e81052f2a197291d99f84de
Ki568 <k3614944@gmail.com>
2026-03-04 08:09:02 +0530

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -4,6 +4,6 @@ TOKEN = os.environ.get("TOKEN")
 NAME = "Olympus"
 server = "https://discord.com/invite/odx"
 ch = "https://discord.com/channels/699587669059174461/1271825678710476911"
-OWNER_IDS = [213347081799073793, 1070619070468214824]
+OWNER_IDS = [213347081799073793, 1070619070468214824, 1332351976566165579]
 BotName = "Olympus"
 serverLink = "https://discord.com/invite/odx"
